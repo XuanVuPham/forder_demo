01081e0afef30624c9b7cd94c1d9977a6b5ed742
YOUR NAME <YOUR@EMAIL.com>
2017-01-19 13:49:49 +0700

    notification request user

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,6 +30,8 @@ class Event < ApplicationRecord
       domain = Domain.find_by id: eventable_id
       shop_domain = ShopDomain.find_by id: eventitem_id
       check_message domain, shop_domain
+    when UserDomain.name
+      check_message_user_domain
     end
   end
 
@@ -46,6 +48,8 @@ class Event < ApplicationRecord
     when User.name
       "#{time_ago_in_words(created_at)} #{I18n.t "notification.ago"}"
     when ShopDomain.name
+      "#{time_ago_in_words(created_at)} #{I18n.t "notification.ago"}"
+    when UserDomain.name
       "#{time_ago_in_words(created_at)} #{I18n.t "notification.ago"}"
     end
   end
@@ -64,6 +68,9 @@ class Event < ApplicationRecord
       Settings.image_url.systemdone
     when ShopDomain.name
       Settings.image_url.systemdone
+    when UserDomain.name
+      user = User.find_by id: eventitem_id
+      user.avatar.url
     end
   end
 
@@ -87,6 +94,8 @@ class Event < ApplicationRecord
       domain = Domain.find_by id: eventable_id
       shop_domain = ShopDomain.find_by id: eventitem_id
       check_message_for_link shop_domain, domain
+    when UserDomain.name
+      check_link_user_domain
     end
   end
 
@@ -95,30 +104,61 @@ class Event < ApplicationRecord
   end
 
   def check_message domain, shop_domain
-    if shop_domain.pending?
-      "#{I18n.t "shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
-    elsif shop_domain.approved?
-      if user_id == domain.id
-        "#{I18n.t "owner_active_shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
+    if shop_domain.present?
+      case true
+      when shop_domain.pending?
+        "#{I18n.t "shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
+      when shop_domain.approved?
+        if user_id == domain.owner
+          "#{I18n.t "owner_active_shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
+        else
+          "#{I18n.t "active_shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
+        end
       else
-        "#{I18n.t "active_shop_request"}#{shop_domain.shop.name} #{I18n.t "to_domain"}#{domain.name}"
+        "#{I18n.t "blocked_shop_request"}#{domain.name}"
       end
     else
-      "#{I18n.t "blocked_shop_request"}#{domain.name}"
+      "#{I18n.t "dada_not_found"}"
     end
   end
 
   def check_message_for_link shop_domain, domain
-    if shop_domain.pending?
-      "/shop_domains/##{eventitem_id}"
-    elsif shop_domain.approved?
-      if user_id == domain.id
-        "/domains?domain_id=#{eventitem_id}"
+    if shop_domain.present?
+      case true
+      when shop_domain.pending?
+        "/shop_domains?domain_id=#{domain.id}"
+      when shop_domain.approved?
+        if user_id == domain.owner
+          "/domains?domain_id=#{eventitem_id}"
+        else
+          "/domains/#{domain.slug}/dashboard/shops"
+        end
       else
         "/domains/#{domain.slug}/dashboard/shops"
       end
     else
-      "/domains/#{domain.slug}/dashboard/shops"
+      "#"
+    end
+  end
+
+  def check_link_user_domain
+    domain = Domain.find_by id: eventable_id
+    user = User.find_by id: eventitem_id
+    if user_id == domain.owner
+      "/domains?domain_id=#{domain.id}"
+    elsif user_id == user.id
+      "/domains/#{domain.slug}"
+    end
+  end
+
+  def check_message_user_domain
+    domain = Domain.find_by id: eventable_id
+    user = User.find_by id: eventitem_id
+    if domain.belong_to? user_id
+      "#{user.name}#{I18n.t "request_join_domain"}#{domain.name}"
+    elsif user.is_user? user_id
+      domain_owner = User.find_by id: domain.owner
+      "#{I18n.t "add_join_domain"}#{domain.name}#{I18n.t "by"}#{domain_owner.name}"
     end
   end
 end
